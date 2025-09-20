5aca8d2bb0e494ef8476f8f04063620e6bfe2966
Hamza Anwar <53189058+hamzaanwar786@users.noreply.github.com>
2025-09-20 17:47:51 +0300

    First Day Work

--- print_1_to_100_with_for_loop.cpp
+++ print_1_to_100_with_for_loop.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    for(int i = 1; i <= 100; i++) {
+        cout << i << endl;
+    }
     return 0;
 }
---
+++ print_name_and_age.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "My name is Hamza Anwar." << endl;
+    cout << "I am 29 years old." << endl;
+    return 0;
+}
